7fa12a425e279ad967132377bdd362e23e54c9b5
Murack-Miller Alexander <826784@stma.k12.mn.us>
2021-09-13 09:45:05 -0500

    yo

---
+++ main.py
@@ -0,0 +1,14 @@
+#print 1st movie 
+print("The Shining")
+
+#print 2nd movie 
+print("The wolf of wallstreet")
+
+#print 3rd movie 
+print("fantastic mr fox")
+
+#print 4th movie 
+print("Avengers Infinity war")
+
+#print 5th movie
+print("FoxCatcher")
