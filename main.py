9d7612d5195733ed16cf53fcb6564863b6a5be7f
Alexmidnighmiller2024 <826784@mystma.org>
2021-09-17 09:03:06 -0500

    MadLib

--- main.py
+++ main.py
@@ -18,3 +18,23 @@ def FiveFavFilms():
 print("These are my top five movies")
 FiveFavFilms()
 
+def TimesTwo(x):
+    print(x * 2)
+
+TimesTwo(2)
+
+def PlusFive(x):
+        print(x + 1072)
+PlusFive(5)
+
+def DivideByTwo(x):
+    print(64 / x)
+DivideByTwo(8)
+
+
+
+def MadLib(NounOne,VerbOne,AdjectiveOne,ColorOne,NounTwo,NounThree,AdjectiveTwo,VerbTwo,PartOfTheBodyOne,ColorTwo):
+    #print first line 
+    print("One Day when i was at the" + NounOne + "when i" + VerbOne + "i looked up at this" + AdjectiveOne + "kid and he said i like your" + ColorOne + "then i siad thanks i like your" + NounTwo + "then aliens started attcking the" + NounThree + "their ships were very" + AdjectiveTwo + "me and the other kid strted" + VerbTwo + "then he got hurt in the" + PartOfTheBodyOne + "so then i grabbed his" + ColorTwo + "shoes and ran off,the end" )
+    print("thisMadLibDoesNotMakeSense")
+ MadLib(NounOne,VerbOne,AdjectiveOne,ColorOne,NounTwo,NounThree,AdjectiveTwo,VerbTwo,PartOfTheBodyOne,ColorTwo)
